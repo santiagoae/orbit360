95902854c038dea983559a65ad2b3c9f7ae85745
santiagoae <santiago_avendano82162@elpoli.edu.co>
2022-10-25 21:34:09 -0500

    mapa v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LobbyComponent } from './components/lobby/lobby.component';
 
 const routes: Routes = [
-  {path: "/", component:LobbyComponent}
+  {path: "lobby", component:LobbyComponent}
 ];
 
 @NgModule({
