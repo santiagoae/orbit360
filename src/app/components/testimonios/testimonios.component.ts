967f3125067ac2a5c0146d70dd5374492b5635c7
santiagoae <santiago_avendano82162@elpoli.edu.co>
2023-01-07 09:52:36 -0500

    planes y dicen carusel horizontal, titulos para los servicios

--- src/app/components/testimonios/testimonios.component.ts
+++ src/app/components/testimonios/testimonios.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { SlickCarouselComponent } from 'ngx-slick-carousel';
 
 @Component({
   selector: 'app-testimonios',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./testimonios.component.scss']
 })
 export class TestimoniosComponent implements OnInit {
+  @ViewChild('slickModal')
+  slickModal!: SlickCarouselComponent;
+  str = 'video' as string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  slides: any[] = [
+    {img:'/assets/comentarios.png' },
+    {img:'/assets/comentarios.png' },
+    {img:'/assets/comentarios.png' },
+    {img:'/assets/comentarios.png' },
+    {img:'/assets/comentarios.png' },
+    {img:'/assets/comentarios.png' },
+    {img:'/assets/comentarios.png' },
+  ];
+  slideConfig = {
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    // dots: true,
+    infinite: false,
+    nextArrow: false,
+    prevArrow: false,
+  };
+
+  data = Object.values(this.slides);
+
+  prevImg() {
+    this.slickModal.slickPrev();
   }
-
+  nextImg() {
+    this.slickModal.slickNext();
+  }
 }
